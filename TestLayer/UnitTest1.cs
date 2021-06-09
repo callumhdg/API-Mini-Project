da34062ae718c1798bf11a935cebe5b13c75ac40
Callum J Harding <callumhdg@gmail.com>
2021-06-09 11:37:52 +0100

    Added Test Layer

---
+++ TestLayer/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace TestLayer
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
