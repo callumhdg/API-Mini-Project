cf8f5bdffa52c004b5391dbd2fb72138922e1719
Callum J Harding <callumhdg@gmail.com>
2021-06-09 15:53:55 +0100

    Added more tests to suubmit joke response

--- TestLayer/Tests/SubmitJokeResponseTest.cs
+++ TestLayer/Tests/SubmitJokeResponseTest.cs
@@ -51,6 +51,72 @@ namespace TestLayer
         {
             Assert.That(_sjs.JsonResponse["message"].ToString(), Is.EqualTo("Joke submission was successfully saved. It will soon be checked out by the author."));
         }
+
+        [Test]
+        public void GivenAValidJoke_CategoryIsMisc()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["category"].ToString(), Is.EqualTo("Misc"));
+        }
+
+        [Test]
+        public void GivenAValidJoke_TypeIsSingle()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["type"].ToString(), Is.EqualTo("single"));
+        }
+
+        [Test]
+        public void GivenAValidJoke_JokeIsCorrect()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["joke"].ToString(), Is.EqualTo("A horse walks into a bar..."));
+        }
+
+        [Test]
+        public void GivenAValidJoke_LangIsEn()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["lang"].ToString(), Is.EqualTo("en"));
+        }
+
+        [Test]
+        public void GivenAValidJoke_FlagsNsfwIsFalse()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["flags"]["nsfw"].ToString().ToLower(), Is.EqualTo("false"));
+        }
+
+        [Test]
+        public void GivenAValidJoke_FlagsReligiousIsFalse()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["flags"]["religious"].ToString().ToLower(), Is.EqualTo("false"));
+        }
+
+        [Test]
+        public void GivenAValidJoke_FlagsPoliticalIsFalse()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["flags"]["political"].ToString().ToLower(), Is.EqualTo("false"));
+        }
+
+        [Test]
+        public void GivenAValidJoke_FlagsRacistIsFalse()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["flags"]["racist"].ToString().ToLower(), Is.EqualTo("false"));
+        }
+
+        [Test]
+        public void GivenAValidJoke_FlagsSexistIsFalse()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["flags"]["sexist"].ToString().ToLower(), Is.EqualTo("false"));
+        }
+
+        [Test]
+        public void GivenAValidJoke_FlagsExplicitIsFalse()
+        {
+            Assert.That(_sjs.JsonResponse["submission"]["flags"]["explicit"].ToString().ToLower(), Is.EqualTo("false"));
+        }
+
+        [Test]
+        public void GivenAValidJoke_TimesampIsNotNull()
+        {
+            Assert.That(_sjs.JsonResponse["timestamp"], Is.Not.Null);
+        }
     }
 
     public class SubmitJokeResponseTest_UnhappyPath
@@ -78,7 +144,23 @@ namespace TestLayer
             Assert.That(_sjs.JsonResponse["error"].ToString().ToLower(), Is.EqualTo("true"));
         }
 
+        [Test]
+        public void GivenAnInvalidJoke_MessageIsCorrect()
+        {
+            Assert.That(_sjs.JsonResponse["message"].ToString(), Is.EqualTo("Malformed Joke"));
+        }
 
+        [Test]
+        public void GivenAnInvalidJoke_CodeIs105()
+        {
+            Assert.That(_sjs.JsonResponse["code"].ToString(), Is.EqualTo("105"));
+        }
+
+        [Test]
+        public void GivenAnInvalidJoke_InternalErrorIsFalse()
+        {
+            Assert.That(_sjs.JsonResponse["internalError"].ToString().ToLower(), Is.EqualTo("false"));
+        }
 
     }
 
