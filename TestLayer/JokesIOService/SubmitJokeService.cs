226aa1c0bade3a2a445f90eec755d3ed20aad79b
Callum J Harding <callumhdg@gmail.com>
2021-06-09 15:53:55 +0100

    Added more tests to suubmit joke response

--- TestLayer/JokesIOService/SubmitJokeService.cs
+++ TestLayer/JokesIOService/SubmitJokeService.cs
@@ -22,41 +22,7 @@ namespace TestLayer
 
         public async Task SubmitJokeRequestAsync(JObject body)
         {
-            //submitJokeResponse = await CallManager.MakeJokeRequestAsync($"submit{values}{}{}{}");
-            //JObject body = new JObject(            
-            //    new JProperty("formatVersion", 3),
-            //    new JProperty("category", "Misc"),
-            //    new JProperty("type", "single"),
-            //    new JProperty("joke", "A horse walks into a bar..."),
-            //    new JArray("flags",
-            //        new JProperty("nsfw", true),
-            //        new JProperty("religious", false),
-            //        new JProperty("political", true),
-            //        new JProperty("racist", false),
-            //        new JProperty("sexist", false),
-            //        new JProperty("explicit", false)
-            //    ),
-            //new JProperty("lang", "en")
-            //);
-
-            //JObject body = new JObject();
-
-            //body["formatVersion"] = 3;
-            //body["category"] = "Misc";
-            //body["type"] = "single";
-            //body["joke"] = "A horse walks into a bar...";
-            //body["flags"] = new JArray { new JObject 
-            //{ ["nsfw"] = false, 
-            //    ["religious"] = false, 
-            //    ["political"] = false,
-            //    ["racist"] = false,
-            //    ["sexist"] = false,
-            //    ["explicit"] = false,
-            //} };
-            //body["lang"] = "en";
-
-
-            submitJokeResponse = await CallManager.SubmitJokeRequestAsync($"submit", body);
+            submitJokeResponse = await CallManager.SubmitJokeRequestAsync("submit", body);
 
             JsonResponse = JObject.Parse(submitJokeResponse);
 
